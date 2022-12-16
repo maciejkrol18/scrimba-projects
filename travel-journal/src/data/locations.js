b0d88107554440eaf4645789033235df3c11c06d
Fancy <maciekgplay@gmail.com>
2022-12-16 13:29:39 +0100

    finishing off
    Added more data, article columns are now equal

--- travel-journal/src/data/locations.js
+++ travel-journal/src/data/locations.js
@@ -9,12 +9,21 @@ export default [
         imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
     },
     {
-        title: "Sydney Opera House",
-        location: "Australia",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+        title: "Tottori Sand Dunes",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/nnjMRah33ebkY3856",
+        startDate: "26 Jan, 2021",
+        endDate: "27 Jan, 2021",
+        description: "The Tottori Sand Dunes are sand dunes located outside the city center of Tottori in Tottori Prefecture, Japan. At a length of 9 miles and less than 1.5 miles wide, it is the largest sand dune in Japan.",
+        imageUrl: "https://images.unsplash.com/photo-1635902918331-10e4952b3920?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+        title: "Kabukicho",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/uEu2Z9YDjLcL4scK9",
+        startDate: "29 Jan, 2021",
+        endDate: "30 Jan, 2021",
+        description: "Kabukicho is a famous entertainment district best known for adult-oriented nightlife. Small clubs, smoky pubs, and late-night snack bars are crammed into lantern-lit alleys in Golden Gai.",
+        imageUrl: "https://images.unsplash.com/photo-1552258694-1d83c5b9d2b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8a2FidWtpY2hvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
     },
 ]
